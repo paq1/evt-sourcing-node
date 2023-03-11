c5afea22e2b2156d5669b113c5c0e9030e0aaf2b
paq1 <32450977+paq1@users.noreply.github.com>
2023-03-11 16:03:00 +0100

    Delete dist directory

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const kafka_engine_1 = require("./kafka-engine/kafka-engine");
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-kafka_engine_1.KafkaEngine.start();
