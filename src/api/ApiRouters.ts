6057bd4dbbf4feb8c8db18f6b27d125405eeb1e2
Pierre Paquin <p.paquin1016@laposte.net>
2023-03-12 13:48:27 +0100

    feat: ajout des routes

---
+++ src/api/ApiRouters.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import {CommandTaskCreate} from "../model/commands/CommandTaskCreate";
+import {randomUUID} from "crypto";
+import {CreateHandler} from "../core/commandHandler/CreateHandler";
+
+export class ApiRouters {
+
+    private app = express();
+
+    private static instance: ApiRouters;
+    private constructor() {
+        this.routes();
+    }
+
+    static start(): ApiRouters {
+        if (!ApiRouters.instance) {
+            ApiRouters.instance = new ApiRouters();
+        }
+        return ApiRouters.instance;
+    }
+
+    private routes(): void {
+        this.app.get('/create', (req, res) => {
+
+            // todo a recup dans le body
+            let commandCreate = new CommandTaskCreate(randomUUID(), "toto");
+
+            // todo injecter la commande dans le topic kafka
+
+
+            // commandHandler dans la partie kafka
+            let commandHandler = new CreateHandler();
+            let evt = commandHandler
+                .toEvent(commandCreate, randomUUID());
+
+            res.send(evt);
+        });
+
+        this.app.listen(3000, () => {
+            console.log(`Application exemple à l'écoute sur le port 3000!`);
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import {KafkaEngine} from "./kafka-engine/kafka-engine";
 import {config} from "dotenv";
 import {CommandTaskCreate} from "./model/commands/CommandTaskCreate";
 import {randomUUID} from "crypto";
+import {ApiRouters} from "./api/ApiRouters";
 
 config();
-
 KafkaEngine.start();
+ApiRouters.start();
 
 let commandCreate = new CommandTaskCreate(randomUUID(), "toto");
 
